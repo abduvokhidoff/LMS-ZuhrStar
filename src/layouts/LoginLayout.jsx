e188b654dc18585b813046f3683130ac66d51341
abduvokhidoff <abcdefgmananam@gmail.com>
2025-11-11 19:00:43 +0500

    'done'

--- src/layouts/LoginLayout.jsx
+++ src/layouts/LoginLayout.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { Eye } from 'lucide-react'
+import React, { useState } from 'react'
 
 const LoginLayout = () => {
+  const [inputType, setInputType] = useState('password')
+  const [eyeType, setEyeType] = useState('Eye')
+
+  const handleSubmit = () => {
+    console.log('Everything is ok')
+  }
   return (
-    <div>LoginLayout</div>
+    <div>
+      <div>
+        <form onSubmit={() => handleSubmit()}>
+          <input type="text" placeholder='Your Phone Number' />
+          <div>
+            <input type={inputType} placeholder='Your Password' />
+            <button>
+              <img src={eyeType} alt="" />
+            </button>
+          </div>
+        </form>
+        <button>
+          
+        </button>
+      </div>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
   )
 }
 
